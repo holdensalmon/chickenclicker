cc7c7c88d35a1a5628a4a4800b6730e42add4868
holdensalmon <holdensalmon@gmail.com>
2018-08-08 12:01:35 -0600

    poop daddy

--- Desktop/NewChicken/Hm/src/components/generator.js
+++ Desktop/NewChicken/Hm/src/components/generator.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import chick from './pics/chicken-sprite-main.png';
 import egg from './pics/upgrade1.png';
 import lamp from './pics/heatLamp.png';
+import mutant from './pics/mutant.png';
 import Upgrade1 from './upgrade1';
 
 var eggCost = 100;
 var lampCost = 250;
+var mutantCost = 375;
 var totalSeconds = 0;
 
 
@@ -18,16 +20,17 @@ class Generator extends Component {
     super();
 
 
-
     this.state = {
       chickCount: 0,
       eggCount: 0,
-      lampCount: 0
+      lampCount: 0,
+      mutantCount: 0
     };
 
     this.click = this.click.bind(this);
     this.eggClick = this.eggClick.bind(this);
     this.lampClick = this.lampClick.bind(this);
+    this.mutantClick = this.mutantClick.bind(this);
     this.incrementSeconds = this.incrementSeconds.bind(this);
   }
 
@@ -35,12 +38,12 @@ class Generator extends Component {
 
   incrementSeconds() {
     totalSeconds += 1;
-    this.setState ({
+    this.setState({
       chickCount: this.state.chickCount + 1
     })
   }
-  
-  
+
+
 
   click() {
     var element = document.getElementById("mainChick");
@@ -52,6 +55,15 @@ class Generator extends Component {
     this.setState({
       chickCount: (this.state.chickCount + 1 + this.state.eggCount)
     })
+    
+    var crit = Math.floor((Math.random() * 10) + 1);
+    console.log(crit);
+
+    if (this.state.mutantCount == 1 && crit <= 3) {
+      this.setState({
+        chickCount: (this.state.chickCount + 2 + this.state.eggCount)
+      })
+    }
   }
 
   shrink() {
@@ -76,13 +88,12 @@ class Generator extends Component {
         eggCount: ++this.state.eggCount
       })
 
-      eggCost = eggCost * 2.5;
+      eggCost = eggCost * 1.35;
+      eggCost = Math.round(eggCost);
     }
   }
 
   lampClick() {
-
-
     var lamp = document.getElementById("lamp");
     lamp.classList.remove("lamp-norm");
     lamp.classList.add("lamp-up");
@@ -98,26 +109,51 @@ class Generator extends Component {
       this.setState({
         lampCount: ++this.state.lampCount
       })
-      lampCost = lampCost * 2.5;
+      lampCost = lampCost * 1.35;
+      lampCost = Math.ceil(lampCost);
+    }
+  }
+
+  mutantClick() {
+    var mutant = document.getElementById("mutant");
+    mutant.classList.remove("mutant-norm");
+    mutant.classList.add("mutant-up");
+    setTimeout(this.Turn, 30);
+    console.log("mutant works");
+
+    if (this.state.chickCount >= mutantCost) {
+      
+      this.setState({
+        chickCount: this.state.chickCount - mutantCost
+      })
+      this.setState({
+        mutantCount: ++this.state.mutantCount
+      })
+      mutantCost = mutantCost * 3;
+      mutantCost = Math.ceil(mutantCost);
     }
   }
 
   Turn() {
     var egg = document.getElementById("egg");
     var lamp = document.getElementById("lamp");
+    var mutant = document.getElementById("mutant");
     egg.classList.remove("egg-up");
     egg.classList.add("egg-norm");
     lamp.classList.remove("lamp-up");
     lamp.classList.add("lamp-norm");
+    mutant.classList.remove("mutant-up");
+    mutant.classList.add("mutant-norm");
   }
 
   render() {
 
     return (
       <div>
-        <p id="clickNum">You currently have {this.state.chickCount} chickens</p>
-        <p id="eggNum">You currently have {this.state.eggCount} eggs, another will cost {eggCost}</p>
-        <p id="lampNum">You currently have {this.state.lampCount} lamps, another will cost {lampCost}</p>
+        <p id="clickNum">You currently have {this.state.chickCount} chickens. <br/> Click on me so I can lay more!</p>
+        <p id="eggNum">You currently have {this.state.eggCount} max egg+, another will cost {eggCost}. <br/>Increase the amount of eggs your chick lays per click</p>
+        <p id="lampNum">You currently have {this.state.lampCount} lamps, another will cost {lampCost}. <br/> Lamps will increase your chickens mood, <br/> making it lay eggs without having to click</p>
+        <p id="mutantNum">You currently have {this.state.mutantCount} mutants, another will cost {mutantCost}. <br/> This gives your chicken the chance to lay a mutant egg, <br/> Mutant chickens are worth two chicks in one! </p>
         <img id="mainChick"
           className="size-norm"
           onClick={this.click}
@@ -128,13 +164,21 @@ class Generator extends Component {
           onClick={this.eggClick}
           src={egg}
         />
-          <img
+        <img
           id="lamp"
           className="egg-norm"
           onClick={this.lampClick}
           src={lamp}
         />
-        <div id='seconds-counter'> </div>
+        <img
+          id="mutant"
+          className="mutant-norm"
+          onClick={this.mutantClick}
+          src={mutant}
+        />
+        <p id="cpc">You get {this.state.lampCount} chickens a second</p>
+        <p id="cps">You get {this.state.eggCount + 1} chickens per click</p>
+
       </div>
     )
   }
